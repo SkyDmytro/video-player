f9b10a3a0d6e45401148633c6de5cfa92adf721d
Dima <skydmytro@gmail.com>
2025-02-04 19:36:54 +0100

    hotfix: small fixes

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -18,11 +18,6 @@ const VideoJsPlayer = ({ options, onReady }: VideoPlayerProps) => {
   useEffect(() => {
     if (!playerRef.current && videoRef.current) {
       const videoElement = document.createElement("video-js");
-      videoElement.classList.add(
-        "video-js",
-        "vjs-big-play-centered",
-        "vjs-theme-city"
-      );
       videoRef.current.appendChild(videoElement);
 
       const player = (playerRef.current = videojs(
